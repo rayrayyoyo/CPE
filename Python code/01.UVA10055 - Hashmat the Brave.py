2cca3291f70bd354dce752421fb4c6b036065192
Ray <a968282@gmail.com>
2026-02-25 22:36:43 +0800

    不用用到try=

--- Python code/01.UVA10055 - Hashmat the Brave.py
+++ Python code/01.UVA10055 - Hashmat the Brave.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
 import sys
 
 for line in sys.stdin:
-    try:
-        parts = line.split()
-        if len(parts) < 2:
-            continue
-            
-        a = int(parts[0])
-        b = int(parts[1])
         
-        print(abs(b - a))
-    except ValueError:
-        break
+    parts = line.split()
+    if len(parts) < 2:
+        continue
+            
+    a = int(parts[0])
+    b = int(parts[1])
+        
+    print(abs(b - a))
