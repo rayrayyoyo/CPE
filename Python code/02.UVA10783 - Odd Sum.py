5aa2fb715655dc5ffa955300247408d0acdc75b8
Ray <a968282@gmail.com>
2026-03-09 22:38:04 +0800

    精簡

--- Python code/02.UVA10783 - Odd Sum.py
+++ Python code/02.UVA10783 - Odd Sum.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-case = int(input())  # 讀取測試案例的數量
-
-for i in range(case):  # 遍歷每個測試案例
-    A, B = int(input()), int(input())  # 讀取範圍的起始值 A 和結束值 B
-
-    sum = 0  # 初始化總和為0
-
-    # 使用 for 循環遍歷範圍 [A, B] 內的數字
+case = int(input())
+for i in range(case):
+    A, B = int(input()), int(input())
+    sum = 0
     for k in range(A, B + 1, 1):
-        if k % 2 == 1:  # 如果數字 k 是奇數
-            sum += k  # 將奇數 k 添加到總和中
-
-    # 輸出結果，注意這裡使用了 f-string 格式化輸出
+        if k % 2 == 1:
+            sum += k
     print(f"Case {i + 1}:", sum)
